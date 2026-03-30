a97399aeb2499609e1ec479c3d746f5f63b0141a
Husniddin06 <154774190+Husniddin06@users.noreply.github.com>
2026-03-30 22:34:16 +1000

    Create config.py

---
+++ config.py
@@ -0,0 +1,25 @@
+# config.py
+
+BOT_TOKEN = "8535086929:AAFOvMBHX8lz42jIc-7UJ2ugGPK84bqwvCQ"
+ADMIN_ID = 1999635628
+
+# VPN subscription link
+VPN_LINK = "https://hirbilon.net/open?sub_url=https://g3.hirbilon.net:443/yessub/p5ln8k1qld9nf0sa"
+
+# Payment info
+CARD_NUMBER = "2200 6706 0039 9582"
+CARD_NAME = "Khusniddin"
+
+TON_WALLET = "UQCe2TzE4kDy9dTgiESd_xCTX9FLrVGm29JX-RqaYKhEr0kT"
+USDT_WALLET = "TWJwXNwMYrGXcnbzs2Q6YeBWm4i8XkBHoh"
+
+# Support
+SUPPORT_USERNAME = "@Husnijan_Axi"
+
+# Tariflar
+TARIFFS = {
+    "1_month": {"name": "1 месяц", "price": 75},
+    "3_months": {"name": "3 месяца", "price": 200},
+    "6_months": {"name": "6 месяцев", "price": 350},
+    "12_months": {"name": "12 месяцев", "price": 500},
+}
